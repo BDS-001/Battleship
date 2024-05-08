f409885a3ba33b6cb24cdc5b35916b536a7036ef
BDS <113209967+BDS-001@users.noreply.github.com>
2024-05-08 19:22:32 -0400

    added location of each cell on board

--- src/ui.js
+++ src/ui.js
@@ -13,8 +13,18 @@ const webpage = (() => {
         board.style.gridTemplateRows = 'repeat(10, 30px)';     // Sets the layout to 10 rows
 
         // Loop to create each cell in the grid
+        let row = 0
+        let col = 0
         for (let i = 0; i < 100; i++) {
             const cell = document.createElement('div');
+            const location = [row, col]
+            if (col >= 9) {
+                row++;
+                col = 0
+            } else {
+                col++;
+            }
+            cell.dataset.location = JSON.stringify(location);
             cell.className = 'grid-cell';
             cell.style.width = '1fr';  // Set the width of each cell
             cell.style.height = '1fr';  // Set the height of each cell
