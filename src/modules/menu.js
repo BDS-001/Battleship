1b52c136df347ea34665876fcdcddad0a069fe98
BDS <113209967+BDS-001@users.noreply.github.com>
2024-06-12 18:12:04 -0400

    changed mode select buttons to img

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -8,9 +8,11 @@ const menu = (() => {
     const generateMenu = (setMode) => {
         const body = document.querySelector('body');
 
-        const solo = document.createElement('button');
-        const vs = document.createElement('button');
+        const solo = document.createElement('img');
+        const vs = document.createElement('img');
 
+        solo.src = '../src/assets/images/solomode.png'
+        vs.src = '../src/assets/images/vsmode.png'
         solo.dataset.mode = 'solo';
         vs.dataset.mode = 'vs';
         solo.className = 'mode-select'
